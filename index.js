7741beeb80f0f7d5aef95e3cdb09b110b33bcd86
Gen <zekabondar760@gmail.com>
2022-04-08 21:41:15 +0300

    Course Function 1

---
+++ index.js
@@ -0,0 +1,284 @@
+                             // Function declaration
+                    //==========================================
+
+
+// function fn(){
+//     const x = 5;
+//     const y =10;
+//     console.log(x + y)
+// }
+//
+
+   // Function expression - переменая либо константа которой присвоена функ
+   //    const f3 = function (){
+   //     console.log('hello')
+   //   }
+   // f3();
+
+
+                    //==========================================
+//                      //function for finding random number
+
+
+
+// let max = 200
+// let min = 100
+// function randomInt(){
+//     let rand = Math.floor(min + Math.random() * (max + 1 - min))
+//     console.log(rand)
+// }
+// randomInt();
+//
+
+
+                     //==========================================
+//                           //Function on many parameters
+
+
+// function shoAllSum(...args){
+//     console.log(args);
+//     let sum = 0;
+//     for (i=0;i<arguments.length;i++){
+//         sum = sum + arguments[i];
+//     }
+//
+//     console.log(sum)
+// }
+// shoAllSum(4,5,6,1,3)
+//
+
+
+
+
+// function f1(x,y){
+//    return x + y;
+// }
+// f1(4,5);
+//
+//  function f2(){
+//      return 5;
+//  }
+//  f2();
+// console.log(5+5+5+f1(2,1))
+//
+//
+
+//      function rando(min,max) {
+//          let rand = min + Math.random() * (max + 1 - min);
+//          return Math.floor(rand)
+//      }
+//       let r = rando(222,333)
+//       console.log(r)
+//
+
+                           //==========================================
+//                                   //Interrupt function
+
+
+//  function IndexOf(arr,num) {
+//      for (i = 0; i < arr.length; i++) {
+//          if (arr[i] === num) return i;
+//          return -1;
+//      }
+//  }
+// let result = IndexOf([40,50,60,70],70)
+// console.log(result)
+//
+//
+//
+//    const arr1 =[ [3,4,5], [6,7,9], [7,7,1]];
+// const arr2 = arr1.filter(function (item){
+//     return (sum(item) % 2 === 0)
+// })
+//   console.log(arr2)
+    //   function sum(arr){
+    //     let summ =0;
+    // for (i=0;i<arr.length;i++){
+    //     summ = summ + arr[i];
+    //   }
+// return summ;
+//   }
+
+
+                       //==========================================
+//                                //Calback function
+
+
+    //
+    // function f1(y) {
+    //   console.log(1)
+    //     y();
+    // }
+    //
+    // function second(a, b){
+    //   console.log(a * b)
+    // }
+    // f1(function () {
+    //   second(2, 10)
+    // });
+    //
+    //
+    // const user = {
+    //   age : 18,
+    //   password : 'asdtdgf',
+    //   agreeToTerm : true
+    // }
+    // function checkPassword(){
+    //   return user.password.length > 6;
+    // }
+    // function checkTerms(){
+    //   return user.agreeToTerm === true;
+    // }
+    // function checkAge(){
+    //   return user.age >= 18;
+    // }
+// console.log(checkAge(user),checkPassword(user))
+
+//
+//      function createValid(...args){
+//   return function (obj){
+//     for (i = 0;i < args.length;i++){
+//       if(args[i](obj) === false) return false;
+//     }
+//     return true;
+//   }
+// }
+// const validator1 = createValid(checkAge, checkPassword, checkTerms)
+// console.log(validator1(user))
+
+
+                         //==========================================
+//                                  //Recursion function
+
+//
+// let t =0;
+//   function fr(){
+//     t++;
+//     console.log(t)
+//       if(t >= 30) return
+//       fr();
+//   }
+//   fr();
+
+//
+//
+// function rf(){
+//   let out ='';
+//   for(i = 1;i<30;i++){
+//     out = out + i + ' ';
+//   }
+//   console.log(out)
+// }
+// rf();
+//
+//
+//
+// let i = 0;
+// let out = '';
+//     function f3() {
+//      i++;
+//     out = out + i + ' ';
+//      if(i >= 30) return;
+//      f3();
+//      }
+//     f3();
+// console.log(out)
+//
+//
+//
+//   function randomInteger(min,max) {
+//       let rand = min + Math.random() * (max + 1 - min);
+//       return Math.floor(rand)
+//   }
+//   let sum = 0;
+// function moneyRecursion(){
+//   let money = randomInteger(0,200)
+//   console.log('add :'+ money)
+//   sum = sum + money;
+//   console.log('my summ :',sum)
+//   if(sum >= 5500) return
+//   moneyRecursion();
+// }
+// moneyRecursion();
+
+//
+//
+//    function moneyCycle(){
+// let summ = 0;
+// while(true){
+//   let money = randomInteger(0,200)
+//   summ = summ + money
+//   console.log('my sum :',summ)
+//   if(summ >= 500)return
+// }
+// }
+// moneyCycle();
+//
+//
+//
+// function factorial(n){
+//   let s = 1;
+//   for (i=1;i<=n;i++){
+//    s = s * i
+//   }
+//   console.log(s)
+// }
+// factorial(5);
+//
+//
+//
+// let s1 = 1;
+// function fact2(n){
+// if(n === 0) return;
+// s1 = s1 * n;
+// fact2(n-1);
+// }
+// fact2(5)
+// console.log(s1);
+
+                          //==========================================
+                                        //closure function
+
+
+//
+//     function createStep(n= 0) {
+//       let count = n;
+//       return function step() {
+//         count++;
+//         console.log(count)
+//       }
+//     }
+//    let step1 = createStep(10);
+//  step1();
+//
+//
+//
+//
+//
+//   function randomI(min,max) {
+//       let rand = min -0.5 + Math.random() * (max -min +1);
+//       return Math.round(rand)
+//     }
+//   function createBeggar() {
+//     let s1 = 0;
+//     return function beggar() {
+//           s1 = s1 + randomI(0, 100)
+//           console.log(s1)
+//           if (s1 >= 250) return
+//           beggar();
+//         }
+//   }
+//   let begg1 = createBeggar();
+//   begg1();
+// //
+//
+//
+//
+//     function createCall(n){
+//         return function (){
+//             console.log(1000 * n)
+//         }
+//     }
+//
+// const calc =createCall(5)
+// calc();
